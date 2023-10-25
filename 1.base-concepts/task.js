663399a7322df0ac3399084a442b8606824f22de
Katerina-Masterina <katerinka_94@mail.ru>
2023-10-25 22:17:00 +0900

    Исправление дз

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,38 +3,32 @@
 function solveEquation(a, b, c) {
 	let arr = [];
 
-	let D = Math.pow(b, 2) - 4 * a * c;
-  let x1;
-  let x2;
+	let d = Math.pow(b, 2) - 4 * a * c;
+	let x1;
+	let x2;
 
-	if (D < 0) {
-		return arr;
-	} else
-	if (D == 0) {
+	if (d == 0) {
 		x1 = -b / (2 * a);
 		arr.push(x1);
-
-		return arr;
-	} else
-	if (D > 0) {
-		x1 = (-b + Math.sqrt(D)) / (2 * a);
-		x2 = (-b - Math.sqrt(D)) / (2 * a);
+	} else if (d > 0) {
+		x1 = (-b + Math.sqrt(d)) / (2 * a);
+		x2 = (-b - Math.sqrt(d)) / (2 * a);
 		arr.push(x1, x2);
-
-		return arr;
 	};
+
+	return arr;
 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
-  let P = (percent/100)/12;
-  let S = amount - contribution;
-  let pay = S * (P + (P / (((1 + P)**countMonths) - 1)));
+	let interestRate = (percent / 100) / 12;
+	let loanBody = amount - contribution;
+	let monthlyPayment = loanBody * (interestRate + (interestRate / (((1 + interestRate) ** countMonths) - 1)));
 
-  let summa = Number((pay * countMonths).toFixed(2));
+	let summa = Number((monthlyPayment * countMonths).toFixed(2));
 
-  return summa;
+	return summa;
 
 
 }
